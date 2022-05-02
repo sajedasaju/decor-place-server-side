c38dd729747a45ba4afbf243cafe54ef77071a01
Sajeda Akhter <sajedasaju85@gmail.com>
2022-05-02 15:03:27 +0600

    update added

--- index.js
+++ index.js
@@ -59,6 +59,22 @@ async function run() {
             res.send(result);
         })
 
+        //update user
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedInventory = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    quantity: updatedInventory.quantity
+                },
+            };
+            const result = await inventoryCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
 
     }
     finally {
